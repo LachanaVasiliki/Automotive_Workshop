93f2945e12c04fdbf4efc77effcb7770e8c09c3c
LachanaVasiliki <56480887+LachanaVasiliki@users.noreply.github.com>
2025-08-26 03:14:27 +0300

    Delete apps.py

--- apps.py
+++
@@ -1,8 +0,0 @@
-# Διαμόρφωση της εφαρμογής "automotiveworkshop"
-
-from django.apps import AppConfig
-
-
-class AutomotiveWorkshopConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'  # Ορισμός του τύπου του πεδίου auto field για την εφαρμογή
-    name = 'automotiveworkshop'  # Ονομασία της εφαρμογής
